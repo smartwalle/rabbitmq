7483c6058008f6c3b4c1180fefa4766aac2d6854
SmartWalle <smartwalle@gmail.com>
2025-11-29 23:42:47 +0800

    route

---
+++ examples/route/consumer.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/smartwalle/rabbitmq"
+	"log"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
+	conn, err := rabbitmq.NewConn("amqp://admin:admin@localhost", rabbitmq.Config{})
+	if err != nil {
+		log.Println("连接 RabbitMQ 异常:", err)
+		return
+	}
+	defer conn.Close()
+	log.Println("连接 RabbitMQ 成功")
+
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Println("创建 Channel 异常:", err)
+		return
+	}
+	defer channel.Close()
+	log.Println("创建 Channel 成功")
+
+	var exchange = "exchange_route"
+
+	if err = channel.ExchangeDeclare(exchange, rabbitmq.ExchangeTypeDirect, false, false, false, false, nil); err != nil {
+		log.Println("创建 Exchange 异常:", err)
+		return
+	}
+
+	queue, err := channel.QueueDeclare("", true, true, false, false, nil)
+	if err != nil {
+		log.Println("创建 Queue 异常:", err)
+		return
+	}
+
+	// TODO 将 key 分别设置为 0 和 1，并且启动 consumer
+	var key = "0"
+	if err = channel.QueueBind(queue.Name, key, exchange, false, nil); err != nil {
+		log.Println("Queue 绑定 Exchange 异常:", err)
+		return
+	}
+
+	messages, err := channel.Consume(queue.Name, "", false, false, false, false, nil)
+	if err != nil {
+		log.Println("接收消息异常:", err)
+		return
+	}
+	for {
+		select {
+		case message, ok := <-messages:
+			if !ok {
+				return
+			}
+			log.Println("收到消息:", string(message.Body))
+			message.Ack(false)
+		}
+	}
+}
---
+++ examples/route/producer.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/smartwalle/rabbitmq"
+	"log"
+	"strconv"
+	"time"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
+	conn, err := rabbitmq.NewConn("amqp://admin:admin@localhost", rabbitmq.Config{})
+	if err != nil {
+		log.Println("连接 RabbitMQ 异常:", err)
+		return
+	}
+	defer conn.Close()
+	log.Println("连接 RabbitMQ 成功")
+
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Println("创建 Channel 异常:", err)
+		return
+	}
+	defer channel.Close()
+	log.Println("创建 Channel 成功")
+
+	var exchange = "exchange_route"
+
+	if err = channel.ExchangeDeclare(exchange, rabbitmq.ExchangeTypeDirect, false, false, false, false, nil); err != nil {
+		log.Println("创建 Exchange 异常:", err)
+		return
+	}
+
+	var idx = 0
+	for {
+		idx++
+
+		// TODO 这里 key 的规则是用 idx 除 2 的余数
+		var key = strconv.Itoa(idx % 2)
+		err = channel.Publish(exchange, key, true, false, rabbitmq.Publishing{
+			Body: []byte(fmt.Sprintf("%s-%d", exchange, idx)),
+		})
+
+		if err != nil {
+			log.Printf("发送消息 %d 异常: %+v \n", idx, err)
+		} else {
+			log.Printf("发送消息 %d 成功 \n", idx)
+		}
+
+		time.Sleep(time.Second)
+	}
+}
