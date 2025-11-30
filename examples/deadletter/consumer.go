419e9bdfc0e651721c4522b5d99ca14482b5c86a
SmartWalle <smartwalle@gmail.com>
2025-11-30 12:08:05 +0800

    dead letter

---
+++ examples/deadletter/consumer.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"github.com/smartwalle/rabbitmq"
+	"log"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
+	conn, err := rabbitmq.NewConn("amqp://admin:admin@localhost", rabbitmq.Config{})
+	if err != nil {
+		log.Println("连接 RabbitMQ 异常:", err)
+		return
+	}
+	defer conn.Close()
+	log.Println("连接 RabbitMQ 成功")
+
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Println("创建 Channel 异常:", err)
+		return
+	}
+	defer channel.Close()
+	log.Println("创建 Channel 成功")
+
+	var exchange = "exchange_dead_letter"
+
+	if err = channel.ExchangeDeclare(exchange, rabbitmq.ExchangeTypeDirect, false, false, false, false, nil); err != nil {
+		log.Println("创建 Exchange 异常:", err)
+		return
+	}
+
+	queue, err := channel.QueueDeclare("", true, true, false, false, nil)
+	if err != nil {
+		log.Println("创建 Queue 异常:", err)
+		return
+	}
+
+	if err = channel.QueueBind(queue.Name, "", exchange, false, nil); err != nil {
+		log.Println("Queue 绑定 Exchange 异常:", err)
+		return
+	}
+
+	messages, err := channel.Consume(queue.Name, "", false, false, false, false, nil)
+	if err != nil {
+		log.Println("接收消息异常:", err)
+		return
+	}
+	for {
+		select {
+		case message, ok := <-messages:
+			if !ok {
+				return
+			}
+			log.Println("收到消息:", string(message.Body))
+			message.Ack(false)
+		}
+	}
+}
---
+++ examples/deadletter/producer.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"github.com/smartwalle/rabbitmq"
+	"log"
+	"time"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
+	conn, err := rabbitmq.NewConn("amqp://admin:admin@localhost", rabbitmq.Config{})
+	if err != nil {
+		log.Println("连接 RabbitMQ 异常:", err)
+		return
+	}
+	defer conn.Close()
+	log.Println("连接 RabbitMQ 成功")
+
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Println("创建 Channel 异常:", err)
+		return
+	}
+	defer channel.Close()
+	log.Println("创建 Channel 成功")
+
+	var args = rabbitmq.NewTable()
+	args["x-dead-letter-exchange"] = "exchange_dead_letter"
+	args["x-dead-letter-routing-key"] = ""
+
+	queue, err := channel.QueueDeclare("", true, true, false, false, args)
+	if err != nil {
+		log.Println("创建 Queue 异常:", err)
+		return
+	}
+	log.Println("创建 Queue 成功")
+
+	var i = 0
+	for {
+		i++
+		err = channel.Publish("", queue.Name, false, false, rabbitmq.Publishing{
+			Expiration: "10000",
+			Body:       []byte(fmt.Sprintf("hello %d", i)),
+		})
+		if err != nil {
+			fmt.Printf("发送消息 %d 发生错误: %v \n", i, err)
+		} else {
+			fmt.Printf("发送消息 %d 成功 \n", i)
+		}
+
+		time.Sleep(time.Second)
+	}
+
+	select {}
+}
